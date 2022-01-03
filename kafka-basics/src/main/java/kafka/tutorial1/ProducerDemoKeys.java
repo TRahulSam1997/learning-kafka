7a47ecfc2ffd956d45dda61cf9018e199f6e7515
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-01-03 15:13:59 +1100

    project refactoring

--- kafka-basics/src/main/java/kafka/tutorial1/ProducerDemoKeys.java
+++ kafka-basics/src/main/java/kafka/tutorial1/ProducerDemoKeys.java
@@ -1,4 +1,4 @@
-package com.learning.kafka.tutorial1;
+package kafka.tutorial1;
 
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -6,11 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
-public class ProducerDemoWithCallback {
-    public static void main(String[] args) {
+public class ProducerDemoKeys {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+        Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
 
         String bootstrapServers = "127.0.0.1:9092";
 
@@ -25,8 +26,15 @@ public class ProducerDemoWithCallback {
 
         for (int i=0; i<10; i++) {
             // Create A Producer Record
+
+            String topic = "first_topic";
+            String value = "hello world " + Integer.toString(i);
+            String key  = "id_" + Integer.toString(i);
+
             ProducerRecord<String, String> record =
-                    new ProducerRecord<String, String>("first_topic", "hello world " + Integer.toString(i));
+                    new ProducerRecord<String, String>(topic, key, value);
+
+            logger.info("Key: " + key);
 
             // Send Data - asynchronous
             producer.send(record, new Callback() {
@@ -42,7 +50,7 @@ public class ProducerDemoWithCallback {
                         logger.error("Error while producing", e);
                     }
                 }
-            });
+            }).get();
         }
 
         producer.flush();
